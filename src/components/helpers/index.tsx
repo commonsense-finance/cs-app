acfb5518c2524c12255a2edff0762026793e2d22
Luciano Insua <lucianoinsua@iMac-de-Usuario.home>
2022-01-17 11:40:35 +0100

    init

--- src/components/helpers/index.tsx
+++ src/components/helpers/index.tsx
@@ -3,6 +3,7 @@ import { tokensImage } from 'src/constants/tokens'
 export const TokenLogo = (props: { symbol: string }) => {
   return (
     <img
+      //@ts-ignore
       src={tokensImage[props.symbol]} //CONTROLADO....
       alt=""
       width="23"
